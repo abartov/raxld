e5198c81ee37260b93d2c6d33e4ff7a713b8efb2
Asaf Bartov <asaf.bartov@gmail.com>
2011-03-01 18:12:12 +0200

    Added some actions and beginnings of LD

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  
+  class Annotation
+    include Spira::Resource
+    type URI.new('http://www.openannotation.org/ns/Annotation')
+    property :target,  :predicate => URI.new('http://www.openannotation.org/ns/hasTarget')
+    property :body,  :predicate => URI.new('http://www.openannotation.org/ns/hasBody')
+    property :title, :predicate => DC.title
+  end
 end
