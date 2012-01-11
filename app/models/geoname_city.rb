9e29925101845db79ca54cbece32219ff3fe2652
Asaf Bartov <asaf.bartov@gmail.com>
2012-01-11 15:10:13 -0800

    - Added in-memory quick lookup of city names

--- app/models/geoname_city.rb
+++ app/models/geoname_city.rb
@@ -1,5 +1,6 @@
 class GeonameCity < ActiveRecord::Base
   @@cities = GeonameCity.find(:all, :select => :label).map(&:label) # init at load time
+  # in-memory boolean lookup function; if it returns true, caller is expected to use activerecord methods to find the actual record, e.g. GeonameCity.find_by_label(:first, name)
   def self.lookup(name)
     return @@cities.include?(name)
   end
