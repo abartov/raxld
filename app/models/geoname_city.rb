53667b5fe53232f6e84aec2969652438e9e0a6c4
Asaf Bartov <asaf.bartov@gmail.com>
2012-01-11 15:07:39 -0800

    - Added lookup in in-memory cache of city names

--- app/models/geoname_city.rb
+++ app/models/geoname_city.rb
@@ -1,2 +1,6 @@
 class GeonameCity < ActiveRecord::Base
+  @@cities = GeonameCity.find(:all, :select => :label).map(&:label) # init at load time
+  def self.lookup(name)
+    return @@cities.include?(name)
+  end
 end
