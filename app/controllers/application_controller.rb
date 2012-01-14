cf6238e8227e3c26e75af3ff2fa1467748de04c4
Asaf Bartov <asaf.bartov@gmail.com>
2012-01-14 12:45:59 -0800

    - Moritz asked nicely, so I'll allow Accept as well...

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ def cors_set_access_control_headers
   headers['Access-Control-Allow-Origin'] = '*'
   headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
   headers['Access-Control-Max-Age'] = "1728000"
-  headers['Access-Control-Allow-Headers'] = "X-Requested-With,Content-Type"
+  headers['Access-Control-Allow-Headers'] = "X-Requested-With,Content-Type,Accept"
   headers['Access-Control-Expose-Headers'] = "Location"
 end
 
@@ -29,7 +29,7 @@ end
 def cors_preflight_check
     headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-    headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version'
+    headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version,Accept'
   if request.method == :options
     headers['Access-Control-Max-Age'] = '1728000'
     render :text => '', :content_type => 'text/plain'
