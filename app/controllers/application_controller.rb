6a1c15dd202698b3cceafd089dc244eb1e4e3cb0
Asaf Bartov <asaf.bartov@gmail.com>
2012-01-13 16:46:12 -0800

    - Switched to Nokogiri for XPath and parsing (Ahh, the relief!) - Added XPath-based annotation logic, part one: the task list for replacement (to avoid altering the word counts before we've gone through all the XPath resolutions!)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 require 'xml/xslt'
+require 'nokogiri'
 require 'rexml/document'
 require 'spira'
 require 'rdf/ntriples'
@@ -49,42 +50,118 @@ def fetch_url(url, options)
     nil
   end
 end
-
   # this routine ASSUMES the annotation constraints are valid XPaths
   def annotate_xml_and_render_html(xml_uri) # gacked from the old code in texts_controller
     xslt = XML::XSLT.new()
     target = AnnotationTargetInfo.find_by_uri(xml_uri) # find annotations for target uri
     unless target.nil? || target.annotations.count == 0
       res = NET::HTTP.get_response(URI.parse(xml_uri))
-      xmldoc = REXML::Document.new res.body
       if res.code == 200
+        nodes_to_replace = []
+        #xmldoc = REXML::Document.new res.body
+        xmldoc = Nokogiri::XML(res.body)
         target.annotations.each do |anno|
-          node = REXML::XPath.first xmldoc, anno.xpath
+          node = xmldoc.xpath(anno.xpath).first
           unless node.nil?
             unless anno.annotation_body.nil? or anno.annotation_body.content.nil? or anno.annotation_body.content.empty?
-              anno_body = REXML::Element.new('OAC_Annotation')
-              anno_body.attributes['class'] = 'OAC_Annotation'
+#              anno_body = NokogiriREXML::Element.new('OAC_Annotation')
+              anno_body = Nokogiri::XML::Element.new 'OAC_Annotation', xmldoc
+              anno_body['class'] = 'OAC_Annotation'
+              #anno_body.attributes['class'] = 'OAC_Annotation' # REXML
               if /.jpg$/.match(anno.body)
-                img = REXML::Element.new('OAC_img')
-                img.attributes['src'] = anno.body
-                anno_body.add img
+                img = Nokogiri::XML::Element.new('OAC_img')
+                #img = REXML::Element.new('OAC_img')
+                img['src'] = anno.body 
+                #img.attributes['src'] = anno.body # REXML
+                anno_body.add_child img
+                anno_body.add img # REXML
               else
                 # assume we just dump the body content as-is in our DIV
-                anno_body.add_text anno.annotation_body.content
+                anno_body.add_child Nokogiri::XML::Text.new anno.annotation_body.content
               end
-              node.parent.insert_after(node, anno_body)
+              # remember the node to replace and the annotation body to replace it with
+              # (we don't do it during the loop to avoid changing the order for later XPath matches)
+              nodes_to_replace << [node, anno_body]
+              #node.parent.insert_after(node, anno_body) # REXML
             end
           end
         end
       end
     end
-    xslt.xml = xmldoc
-    xslt.xsl = REXML::Document.new File.read(::Rails.root.to_s+'/public/'+"min.xsl")
-    #xslt.xsl = REXML::Document.new File.read( ::Rails.root.to_s+'/public/'+"tei.xsl")
-    xhtml = xslt.serve()
-    #temporary, fugly hack
-    xhtml.gsub!(/\n/, '<br/>')
+#    xslt.xml = xmldoc
+#    xslt.xsl = REXML::Document.new File.read(::Rails.root.to_s+'/public/'+"min.xsl")
+#    #xslt.xsl = REXML::Document.new File.read( ::Rails.root.to_s+'/public/'+"tei.xsl")
+#    xhtml = xslt.serve()
+#    #temporary, fugly hack
+#    xhtml.gsub!(/\n/, '<br/>')
+     xhtml = nodes_to_replace.to_s
     return xhtml
   end
-end
 
+  # Dirk Roorda's word counting code (originally in Perl)
+  def find_charpos_by_word_number(text, word_number)
+	elementstack = Array.new
+	wordpos = 0;
+	charpos = 0;
+	rest = text
+	while wordpos < word_number and rest != "" 
+		if rest =~ /<[^>]+>/ then
+			pre = $`;
+			tag = $&;
+			rest = $';
+		else
+			pre = rest
+			tag = ""
+			rest = ""
+		end
+		if not elementstack.empty? then
+			charpos += pre.length
+		else 
+			prerep = pre;
+			prerep.sub(/^\s+/,'')
+			prerep.sub(/\s+$/,'')
+			prewords = prerep.split(/\s+/)
+			if prewords.length < word_number - wordpos then
+				charpos += pre.length;
+				wordpos += prewords.length;
+			else
+				rest = pre;
+				while wordpos < word_number and rest != ""
+					if rest =~ /^\S+|\s+/
+						item = $&
+						rest = $'
+					else
+						item = rest
+						rest = ''
+					end
+					isblank = item =~ /\s/
+					if not isblank then
+						wordpos += 1
+						if wordpos < word_number then
+							charpos += item.length
+						end
+					else
+						charpos += item.length
+					end
+				end
+			end
+		end
+		if wordpos < word_number then
+			if tag != "" then
+				charpos += tag.length
+				tag =~ /^<([!?\/]?)([^\s>]*)/
+				close = $1
+				name = $2
+				if close == "/" then
+					elementstack.pop
+				elsif close == "" then
+					if not (tag =~ /\/\s*>$/) then
+						elementstack.push(name)
+					end
+				end
+			end
+		end
+	end
+	charpos
+  end
+end
